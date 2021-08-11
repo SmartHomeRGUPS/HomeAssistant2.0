625ab2449c2862a6db79811b0d4e254a124772a3
root <root@core-ssh.local.hass.io>
2021-08-11 20:52:59 +0300

    some fix

--- www/tileboard/config.js
+++ www/tileboard/config.js
@@ -36,7 +36,7 @@ var CONFIG = {
       right: [
          {
             type: HEADER_ITEMS.CUSTOM_HTML,
-            html: 'MyCity'
+            html: 'Average Temperature In Room'
          },
          {
             type: HEADER_ITEMS.WEATHER,
@@ -90,7 +90,7 @@ var CONFIG = {
       left: [
          {
             type: HEADER_ITEMS.CUSTOM_HTML,
-            html: '<b><i>Home Sweet Home</b></i>'
+            html: '<b><i>SmartHy</b></i>'
          },
          {
             type: HEADER_ITEMS.DATETIME,
@@ -129,203 +129,84 @@ var CONFIG = {
 
    pages: [
       {
-         title: 'Main page',
+         title: 'Main Room',
          bg: 'images/bg1.jpeg',
-         icon: 'mdi-home-outline', // home icon
+         icon: 'mdi-numeric-1-box-outline',
          groups: [
             {
-               title: 'First group',
-               width: 2,
-               height: 3,
-               // row: 0,  // optional; index of the row used for the GRID layout. If not specified, the default is 0
+               title: 'Light',
+               height: 2,
+               width: 3,
                items: [
                   {
-                     position: [0, 0],
+                     position: [0,0],
                      width: 2,
-                     type: TYPES.TEXT_LIST,
-                     id: {}, // using empty object for an unknown id
-                     state: false, // disable state element
-                     list: [
-                        {
-                           title: 'Sun.sun state',
-                           icon: 'mdi-weather-sunny',
-                           value: '&sun.sun.state'
-                        },
-                        {
-                           title: 'Custom',
-                           icon: 'mdi-clock-outline',
-                           value: 'value'
-                        }
-                     ]
-                  },
-                  {
-                     position: [0, 1], // [x, y]
-                     width: 1,
-                     type: TYPES.SENSOR,
-                     id: 'binary_sensor.updater',
-                     state: '@attributes.release_notes' // https://github.com/resoai/TileBoard/wiki/Templates
-                  },
-                  {
-                     title: 'Heating',
-                     position: [1, 1],
-                     id: { attributes: { current_temperature: 11 } },
-                     type: TYPES.CLIMATE,
-                     unit: '°C',
-                     useHvacMode: true,  // Optional: enables HVAC mode (by default uses PRESET mode)
-                     state: function (item, entity) {
-                        return 'Actual: ' + this.$scope.filterNumber(entity.attributes.current_temperature, 1) + ' °C';
-                     },
-                     states: {
-                        'auto': 'Automatic',
-                        'heat': 'Manual',
-                        'off': 'Off'
-                     },
-                  },
-               ]
-            },
-
-            {
-               title: 'Second group',
-               width: 2,
-               height: 3,
-               // row: 0,  // optional; index of the row used for the GRID layout. If not specified, the default is 0
-               items: [
-                  {
-                     position: [0, 0],
-                     width: 1,
-                     type: TYPES.SLIDER,
-                     //id: "input_number.volume",
-                     id: {state: 50, attributes: {}}, // replace it with real string id
-                     state: false,
-                     title: 'Custom slider',
-                     subtitle: 'Example of subtitle',
-                     slider: {
-                        min: 0,
-                        max: 100,
-                        step: 2,
-                        request: {
-                           type: "call_service",
-                           domain: "input_number",
-                           service: "set_value",
-                           field: "value"
-                        }
-                     }
-                  },
-                  {
-                     position: [1, 0],
-                     width: 1,
                      type: TYPES.SWITCH,
-                     //id: "switch.lights",
-                     id: {state: 'off'}, // replace it with real string id (e.g. "switch.lights")
-                     state: false,
-                     title: 'Custom switch',
-                     icons: {'off': 'mdi-volume-off', 'on': 'mdi-volume-high'}
+                     id: "group.all_lights",
+                     title: 'AllLight',
+                     states: {
+                        on: "on",
+                        off: "off",
+                     },
+                     icons: {
+                        on: 'mdi-lightbulb-on',
+                        off: "mdi-lightbulb",
+                     },
                   },
                   {
-                     position: [0, 1],
-                     type: TYPES.ALARM,
-                     // id: "alarm_control_panel.home_alarm",
-                     id: { state: 'disarmed' }, // replace it with real string id
-                     title: 'Home Alarm',
-                     icons: {
-                        arming: 'mdi-bell-outline',
-                        disarmed: 'mdi-bell-off',
-                        pending: 'mdi-bell',
-                        armed_custom_bypass: 'mdi-bell-check',
-                        armed_home: 'mdi-bell-plus',
-                        armed_night: 'mdi-bell-sleep',
-                        armed_away: 'mdi-bell',
-                        triggered: 'mdi-bell-ring',
-                     },
+                     position: [0,1],
+                     width: 2,
+                     type: TYPES.SWITCH,
+                     id: "group.auditorium_lights",
+                     title: "Light in Main Room",
                      states: {
-                        arming: 'Arming',
-                        disarmed: 'Disarmed',
-                        pending: 'Pending',
-                        armed_custom_bypass: 'Armed custom bypass',
-                        armed_home: 'Armed home',
-                        armed_night: 'Armed night',
-                        armed_away: 'Armed away',
-                        triggered: 'Triggered',
+                        on: 'on',
+                        off: 'off',
+                     },
+                     icons: {
+                        on: 'mdi-lightbulb-on',
+                        off: "mdi-lightbulb",
                      },
                   },
-
+                  {
+                     position: [0,2],
+                     width: 2,
+                     type: TYPES.SWITCH,
+                     id: "group.glass_lights",
+                     title: "Light in Glass Room",
+                     states: {
+                        on: 'on',
+                        off: 'off',
+                     },
+                     icons: {
+                        on: 'mdi-lightbulb-on',
+                        off: "mdi-lightbulb",
+                     },
+                  },
                ],
             },
-
             {
-               title: 'MyCity',
-               width: 1,
-               height: 3,
-               // row: 1,  // optional; index of the row used for the GRID layout. If not specified, the default is 0
+               title: 'Sensors',
+               height: 2,
+               width: 3,
                items: [
                   {
-                     // please read README.md for more information
-                     // this is just an example
-                     position: [0, 0],
-                     height: 2, // 1 for compact
-                     //classes: ['-compact'],
-                     type: TYPES.WEATHER,
-                     id: {},
-                     state: function () {return 'Clear, night'}, // https://github.com/resoai/TileBoard/wiki/Anonymous-functions
-                     icon: 'clear-night',
-                     icons: {
-                        'clear-day': 'clear',
-                        'clear-night': 'nt-clear',
-                        'cloudy': 'cloudy',
-                        'exceptional': 'unknown',
-                        'fog': 'fog',
-                        'hail': 'sleet',
-                        'lightning': 'chancestorms',
-                        'lightning-rainy': 'tstorms',
-                        'partly-cloudy-day': 'partlycloudy',
-                        'partly-cloudy-night': 'nt-partlycloudy',
-                        'pouring': 'rain',
-                        'snowy': 'snow',
-                        'snowy-rainy': 'sleet',
-                        'wind': 'unknown',
-                        'windy': 'unknown',
-                        'windy-variant': 'unknown'
-                     },
+                     position: [0,0],
+                     type: TYPES.SENSOR_ICON,
+                     id: 'binary_sensor.water_leak_sensor_1f28',
+                     title: 'water check',
                      states: {
-                        'clear-night': 'Clear, night',
-                        'cloudy': 'Cloudy',
-                        'exceptional': 'Exceptional',
-                        'fog': 'Fog',
-                        'hail': 'Hail',
-                        'lightning': 'Lightning',
-                        'lightning-rainy': 'Lightning, rainy',
-                        'partlycloudy': 'Partly cloudy',
-                        'pouring': 'Pouring',
-                        'rainy': 'Rainy',
-                        'snowy': 'Snowy',
-                        'snowy-rainy': 'Snowy, rainy',
-                        'sunny': 'Sunny',
-                        'windy': 'Windy',
-                        'windy-variant': 'Windy'
+                        on: "On",
+                        off: "Off"
                      },
-                     fields: {
-                        temperature: '18',
-                        temperatureUnit: '°C',
-                        windSpeed: '5',
-                        windSpeedUnit: 'kmh',
-                        humidity: '50',
-                        humidityUnit: '%',
-                        pressure: '650',
-                        pressureUnit: 'hPa',
-                        list: [
-                           'Feels like 16 °C',
-                           /*
-                           'Cloud coverage '
-                              + '&sensor.openweathermap_cloud_coverage.state'
-                              + '&sensor.openweathermap_cloud_coverage.attributes.unit_of_measurement',
-                           */
-                        ]
-                     }
+                     icons: {
+                        on: 'mdi-hot-tub',
+                        off: 'mdi-hot-tub'
+                     },
                   }
-
-               ]
-            }
-         ]
+               ],
+            },
+         ],
       },
       {
          title: 'Second page',
@@ -333,87 +214,9 @@ var CONFIG = {
          icon: 'mdi-numeric-2-box-outline',
          groups: [
             {
-               title: '',
-               width: 4,
-               height: 3,
-               items: [
-                  {
-                     position: [0, 0],
-                     width: 2,
-                     title: 'Short instruction',
-                     type: TYPES.TEXT_LIST,
-                     id: {}, // using empty object for an unknown id
-                     state: false, // disable state element
-                     list: [
-                        {
-                           title: 'Read',
-                           icon: 'mdi-numeric-1-box-outline',
-                           value: 'README.md'
-                        },
-                        {
-                           title: 'Ask on forum',
-                           icon: 'mdi-numeric-2-box-outline',
-                           value: 'home-assistant.io'
-                        },
-                        {
-                           title: 'Open an issue',
-                           icon: 'mdi-numeric-3-box-outline',
-                           value: 'github.com'
-                        },
-                     ]
-                  },
-                  {
-                     position: [2, 0],
-                     width: 2,
-                     title: 'System Status',
-                     type: TYPES.TEXT_LIST,
-                     id: {}, // using empty object for an unknown id
-                     state: false, // disable state element
-                     list: [
-                        {
-                           title: 'Free Memory',
-                           icon: 'mdi-memory',
-                           value: function() {
-                              // var freeMemory = this.parseFieldValue('&sensor.memory_free.state')
-                              var freeMemory = 15.444  // Just an example.
-                              return this.$scope.filterNumber(freeMemory, 1) + ' GB';
-                           }
-                        },
-                     ]
-                  },
-                  {
-                     position: [0, 1.5],
-                     width: 1.5,
-                     height: 1.5,
-                     title: 'My Gauge Title',
-                     subtitle: '',
-                     type: TYPES.GAUGE,
-                     // id: 'sensor.my_sample_sensor', // Assign the sensor you want to display on the gauge
-                     id: {state: 11111}, // Remove after choosing to actual sensor ID
-                     value: function(item, entity) {
-                        return entity.state;
-                     },
-                     settings: {
-                        size: 200, // Defaults to 50% of either height or width, whichever is smaller
-                        type: 'full', // Options are: 'full', 'semi', and 'arch'. Defaults to 'full'
-                        min: 0, // Defaults to 0
-                        max: 25000, // Defaults to 100
-                        cap: 'round', // Options are: 'round', 'butt'. Defaults to 'butt'
-                        thick: 8, // Defaults to 6
-                        label: 'My Gauge', // Defaults to undefined
-                        append: '@attributes.unit_of_measurement', // Defaults to undefined
-                        prepend: '$', // Defaults to undefined
-                        duration: 1500, // Defaults to 1500ms
-                        thresholds: { 0: { color: 'green'}, 80: { color: 'red' } }, // Defaults to undefined
-                        labelOnly: false, // Defaults to false
-                        foregroundColor: 'rgba(0, 150, 136, 1)', // Defaults to rgba(0, 150, 136, 1)
-                        backgroundColor: 'rgba(0, 0, 0, 0.1)', // Defaults to rgba(0, 0, 0, 0.1)
-                        fractionSize: 0, // Number of decimal places to round the number to. Defaults to current locale formatting
-                     },
-                  },
-               ]
-            },
+
+            }
          ]
-      }
+      },
    ],
 }
